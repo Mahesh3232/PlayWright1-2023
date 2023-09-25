46221edd43a3707b50d0557be4d22e6005b5a808
Mahesh3232 <mahesh.aher1999@gmail.com>
2023-09-25 21:18:43 +0530

    Dropdown

--- tests/TC03Dropdown.spec.js
+++ tests/TC03Dropdown.spec.js
@@ -13,6 +13,18 @@ test('Verify the static dropdown',async({page})=>{
 //dynyamic dropdown/Auto suggetion dropdown
 test.only('Verify the Dynyamic Dropdoen',async({page})=>{
     await page.goto('https://www.redbus.in/')
-    await page.locator('#src').fill('Pune')
+    await page.locator('#src').fill('Pune',{delay:1000})
+    await page.waitForSelector('.placeHolderMainText')
+    const optionCount = await page.locator('.placeHolderMainText').count()
+    console.log(optionCount)
+    for(let i = 0 ;i < optionCount;i++){
+        let text = await page.locator('.placeHolderMainText').nth(i).textContent()
+        if(text === 'Nashik Phata'){
+            await page.locator('.placeHolderMainText').nth(i).click()
+            break
+        }
+    }
+    // let text = await page.locator('.placeHolderMainText').allTextContents()
+    // console.log(text)
     await page.pause()
 })
