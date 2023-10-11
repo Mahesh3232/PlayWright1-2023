502cb954fbf6ba2eaf77b18a6452202d7f5be196
Mahesh3232 <mahesh.aher1999@gmail.com>
2023-10-11 16:46:28 +0530

    UpdateDropdown

--- tests/TC03Dropdown.spec.js
+++ tests/TC03Dropdown.spec.js
@@ -11,15 +11,28 @@ test('Verify the static dropdown',async({page})=>{
 })
 
 //dynyamic dropdown/Auto suggetion dropdown
-test('Verify the Dynyamic Dropdoen',async({page})=>{
+test.only('Verify the Dynyamic Dropdoen',async({page})=>{
     await page.goto('https://www.redbus.in/')
     await page.locator('#src').fill('Pune',{delay:1000})
     await page.waitForSelector('.placeHolderMainText')
     const optionCount = await page.locator('.placeHolderMainText').count()
     console.log(optionCount)
     for(let i = 0 ;i < optionCount;i++){
-        let text = await page.locator('.placeHolderMainText').nth(2).textContent()
+        let text = await page.locator('.placeHolderMainText').nth(i).textContent()
         if(text === 'Nashik Phata'){
+            await page.locator('.placeHolderMainText').nth(i).click()
+            break
+        }
+    }
+    page.waitForTimeout(7000)
+    await page.locator('#dest').fill('Mumbai',{delay:3000})
+    page.waitForTimeout(7000)
+    await page.waitForSelector('.sc-iwsKbI.jTMXri >.sc-gZMcBi.grvhsy > .placeHolderMainText')
+    const desoptionCount = await page.locator('.sc-iwsKbI.jTMXri >.sc-gZMcBi.grvhsy > .placeHolderMainText').count()
+    console.log(desoptionCount)
+    for(let i = 0 ;i < desoptionCount;i++){
+        let text = await page.locator('.placeHolderMainText').nth(i).textContent()
+        if(text === 'Mumbai International Airport'){
             await page.locator('.placeHolderMainText').nth(i).click()
             break
         }
