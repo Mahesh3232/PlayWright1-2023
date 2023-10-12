ec4c228dd981fe0261e2f2d160dc80f4bfca00f8
Mahesh3232 <mahesh.aher1999@gmail.com>
2023-10-12 21:27:15 +0530

    Hooks

---
+++ tests/TC08Hooks.spec.js
@@ -0,0 +1,31 @@
+//beforeEach ==> runs before every  Test Block 
+//AfterEach  ==> runs after every Test Block 
+//beforeAll ==> Run only 1time before all test block 
+//AfterAll ==> Run only 1 time after all test black 
+const { test, expect } = require('@playwright/test')
+let page
+
+test.beforeEach(async ({ browser }) => {
+    page = await browser.newPage()
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.locator('input[name="username"]').fill('Admin')
+    await page.locator('input[name="password"]').fill('admin123')
+    await page.locator('button[type="submit"]').click()
+})
+
+test.afterEach(async()=>{
+    await page.locator('p[class="oxd-userdropdown-name"]').click()
+    await page.locator('a[href="/web/index.php/auth/logout"]').click()
+    await page.waitForTimeout(5000)
+    //await console.log('I am from afterEach')
+})
+test("Verify the login functionality with valid credentials", async () => { 
+    await expect(page.locator('span[class="oxd-text oxd-text--span oxd-main-menu-item--name"]').first()).toBeVisible()
+    await expect(page.locator('img[alt="client brand banner"]')).toBeVisible()
+    await expect(page.locator('.oxd-topbar-header-breadcrumb-module')).toHaveText('Dashboard') 
+})
+
+test('Verify the login functionality with invalid credentials', async () => {
+    await expect(page).toHaveTitle('OrangeHRM')
+    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+})
