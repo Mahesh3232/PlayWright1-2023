01776b8731b2b0778bc71fc58b7ec7280ab1936b
Mahesh3232 <mahesh.aher1999@gmail.com>
2023-10-18 21:08:52 +0530

    Hooks

--- tests/TC08Hooks.spec.js
+++ tests/TC08Hooks.spec.js
@@ -3,10 +3,9 @@
 //beforeAll ==> Run only 1time before all test block 
 //AfterAll ==> Run only 1 time after all test block 
 const { test, expect } = require('@playwright/test')
-
 let page
 
-// test.beforeEach(async ({ browser }) => {
+// test.beforeEach(async({browser})=>{
 //     page = await browser.newPage()
 //     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 //     await page.locator('input[name="username"]').fill('Admin')
@@ -15,10 +14,8 @@ let page
 // })
 
 // test.afterEach(async()=>{
-//     await page.locator('p[class="oxd-userdropdown-name"]').click()
-//     await page.locator('a[href="/web/index.php/auth/logout"]').click()
-//     await page.waitForTimeout(5000)
-//     //await console.log('I am from afterEach')
+//     await page.locator('.oxd-userdropdown-name').click()
+//     await page.locator('[href="/web/index.php/auth/logout"]').click()
 // })
 
 test.beforeAll(async ({ browser }) => {
@@ -30,18 +27,21 @@ test.beforeAll(async ({ browser }) => {
 })
 
 test.afterAll(async () => {
-    await page.locator('p[class="oxd-userdropdown-name"]').click()
-    await page.locator('a[href="/web/index.php/auth/logout"]').click()
-    await page.waitForTimeout(4000)
+    await page.locator('.oxd-userdropdown-name').click()
+    await page.locator('[href="/web/index.php/auth/logout"]').click()
+    await page.waitForTimeout(3000)
 })
 
-test("Verify the login functionality with valid credentials", async () => {
+test('Verify the logo on homeoage', async () => {
     await expect(page.locator('span[class="oxd-text oxd-text--span oxd-main-menu-item--name"]').first()).toBeVisible()
     await expect(page.locator('img[alt="client brand banner"]')).toBeVisible()
     await expect(page.locator('.oxd-topbar-header-breadcrumb-module')).toHaveText('Dashboard')
 })
 
-test('Verify the login functionality with invalid credentials', async () => {
-    await expect(page).toHaveTitle('OrangeHRM')
-    await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+test('Verify the URL and title of the page', async () => {
+    await expect(page.locator('.oxd-alert-content-text')).toBeVisible()
+    await expect(page.locator('.oxd-alert-content-text')).toHaveText('Invalid credentials')
+    // await expect(page).toHaveTitle('OrangeHRM')
+    // await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
 })
+
